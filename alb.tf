70b0ad267cd20f7554ecad2a2fbfc743e83ebb16
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-15 23:06:04 +0530

    ${var.ENV}

--- alb.tf
+++ alb.tf
@@ -1,17 +1,17 @@
 resource "aws_lb" "public" {
-  name               = "${ENV}-public-alb"
+  name               = "${var.ENV}-public-alb"
   internal           = false
   load_balancer_type = "application"
 #  security_groups    = [aws_security_group.lb_sg.id]
   subnets            = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNETS
 
   tags = {
-    Environment = "${ENV}-public-alb"
+    Environment = "${var.ENV}-public-alb"
   }
 }
 
 resource "aws_lb" "private" {
-  name               = "${ENV}-private-tf"
+  name               = "${var.ENV}-private-tf"
   internal           = true
   load_balancer_type = "application"
 #  security_groups    = [aws_security_group.lb_sg.id]
@@ -19,6 +19,6 @@ resource "aws_lb" "private" {
 
 
   tags = {
-    Environment = "${ENV}-private-tf"
+    Environment = "${var.ENV}-private-tf"
   }
 }
